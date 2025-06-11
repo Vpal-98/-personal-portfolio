20d70400b3b6d19d638ab0dfd32d9b02de62d913
Vpal72061@gmail.com <Vpal72061@gmail.com>
2025-06-11 16:29:34 +0530

    skills section

---
+++ asstes/js/main.js
@@ -0,0 +1,17 @@
+
+
+
+// scroll about animation
+gsap.registerPlugin(ScrollTrigger);
+gsap.utils.toArray(".text-gradient").forEach((span) => {
+  gsap.to(span, {
+    backgroundSize: "100% 100%",
+    ease: "none",
+    scrollTrigger: {
+      trigger: span,
+      start: "top bottom",
+      end: "top center",
+      scrub: true,
+    },
+});
+});
